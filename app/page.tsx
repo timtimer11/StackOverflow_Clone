3d7bb6fc00873dd7518630d696e1a83586d6d32a
timtimer11 <justanestudio@gmail.com>
2024-10-26 16:14:51 +0700

    Added assets, fonts, and icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 // import Image from "next/image";
 
-const Home = () => {
-  return <h1 className="h1-bold">Hello World</h1>;
-};
+const Home = () => (
+  <>
+    <h1 className="h1-bold">Hello World</h1>;
+    <h1 className="h1-bold font-inter">Hello World (INTER)</h1>;
+    <h1 className="h1-bold font-space-grotesk">Hello World (INTER)</h1>;
+  </>
+);
 
 export default Home;

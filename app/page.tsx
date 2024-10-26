ba285821ae0ba58339c11af53db358a9b68a79b5
timtimer11 <justanestudio@gmail.com>
2024-10-26 15:08:48 +0700

    ESLint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 // import Image from "next/image";
 
 const Home = () => {
-  return <h1>Hello World</h1>;
+  return <h1 className="text-3xl font-bold text-white">Hello World</h1>;
 };
 
 export default Home;
